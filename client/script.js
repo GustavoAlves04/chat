70acfbd0e9c1abeac257c626d31aefb0df5bbda9
GustavoAlves04 <gustavoalcontato@gmail.com>
2023-04-05 15:52:36 -0300

    js

---
+++ client/script.js
@@ -0,0 +1,16 @@
+
+function changeTitle(){
+    document.getElementById("title").innerHTML = "Front-End Developers"   
+}
+
+function changeTitle2(){
+    document.getElementById("title").innerHTML = "Back-End Developers"
+}
+
+function changeTitle3(){
+    document.getElementById("title").innerHTML = "Random"
+}
+
+function changeTitle4(){
+    document.getElementById("title").innerHTML = "Welcome"
+}
